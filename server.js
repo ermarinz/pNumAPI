1c18f611dc1e81edf0fc6d485eb2bd967647cc6d
ermarinz <evan.marinzel@gmail.com>
2018-02-04 17:06:31 -0500

    Added POST route to extract phone numbers from base64 body text

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const PNF = require('google-libphonenumber').PhoneNumberFormat;
 const phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance();
+const bodyParser = require('body-parser');
 const app = express();
+const textParser = bodyParser.text();
 
 const HTTP_PORT = process.env.PORT || 8080;
 
@@ -10,30 +12,40 @@ function onHttpStart() {
     console.log("Express http server listening on: " + HTTP_PORT);
 }
 
-// Helper function used to avoid processing duplicate phone numbers.
+// Used to avoid processing duplicate phone numbers.
 function onlyUnique(value, index, self) { 
     return self.indexOf(value) === index;
 }
 
-// GET route handler for parsing text by URL.
-app.get("/api/phonenumbers/parse/text/:phoneString", (req, res) => {
-    const input = req.params.phoneString;
-    let possibleNumbers = input.match(/[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}/g);
+// Used to find valid phone numbers within inputText.
+function findNumbers(inputText) {
     let foundNumbers = [];
+    let possibleNumbers = inputText.match(/[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}/g);
     if (possibleNumbers) {
         for (let i = 0; i < possibleNumbers.length; i++) 
             possibleNumbers[i] = possibleNumbers[i].replace(/\D/g, ""); // Removes formatting from possible numbers
         possibleNumbers = possibleNumbers.filter( onlyUnique );         // Removes duplicates
         for (let i = 0; i < possibleNumbers.length; i++) {
-            const number = phoneUtil.parseAndKeepRawInput(possibleNumbers[i], 'US');
+            const number = phoneUtil.parseAndKeepRawInput(possibleNumbers[i], 'US');    // Parse to libphonenumber
             if (phoneUtil.isValidNumber(number))
-                foundNumbers.push(phoneUtil.formatInOriginalFormat(number, 'US'));    // Possible number is a valid phone number
+                foundNumbers.push(phoneUtil.formatInOriginalFormat(number, 'US'));      // Possible number is a valid phone number
         }
     }
-    res.json(foundNumbers);
+    return foundNumbers;
+}
+
+// GET route handler for parsing text by URL.
+app.get("/api/phonenumbers/parse/text/:phoneString", (req, res) => {
+    res.json(findNumbers(req.params.phoneString));
 });
 
-// TODO: POST route handler for parsing input by text file.
+// POST route handler for parsing input by text file.
+app.post("/api/phonenumbers/parse/:file", textParser, (req, res) => {
+    if (!req.body) 
+        return res.sendStatus(400).send("Error reading text file")
+    const decodedText = Buffer.from(req.body, 'base64').toString("ascii");    //decode base64 text
+    res.json(findNumbers(decodedText));
+});
 
 // Send 404 response code for undefined routes.
 app.use ((req, res) => {
